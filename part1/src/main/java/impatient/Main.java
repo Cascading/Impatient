909d923b2c24576fb46080fe00590f0509ba472b
ceteri <ceteri@gmail.com>
2012-06-28 15:19:47 -0700

    adding Apache license

--- part1/src/main/java/impatient/Main.java
+++ part1/src/main/java/impatient/Main.java
@@ -1,7 +1,21 @@
 /*
  * Copyright (c) 2007-2012 Concurrent, Inc. All Rights Reserved.
  *
- * Project and contact information: http://www.concurrentinc.com/
+ * Project and contact information: http://www.cascading.org/
+ *
+ * This file is part of the Cascading project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package impatient;
@@ -15,7 +29,7 @@ import cascading.pipe.Pipe;
 import cascading.property.AppProps;
 import cascading.scheme.hadoop.TextDelimited;
 import cascading.tap.Tap;
-import cascading.tap.hadoop.Lfs;
+import cascading.tap.hadoop.Hfs;
 import cascading.tuple.Fields;
 
 
@@ -33,10 +47,10 @@ public class
     HadoopFlowConnector flowConnector = new HadoopFlowConnector( properties );
 
     // create the source tap
-    Tap inTap = new Lfs( new TextDelimited( true, "\t" ), inPath );
+    Tap inTap = new Hfs( new TextDelimited( true, "\t" ), inPath );
 
     // create the sink tap
-    Tap outTap = new Lfs( new TextDelimited( true, "\t" ), outPath );
+    Tap outTap = new Hfs( new TextDelimited( true, "\t" ), outPath );
 
     // specify a pipe to connect the taps
     Pipe simplePipe = new Pipe( "simple" );
