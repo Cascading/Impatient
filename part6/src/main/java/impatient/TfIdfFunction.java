edfbf73c1a98cd8f75fc44e02325ad49c80c7bd4
ceteri <ceteri@gmail.com>
2012-07-05 15:03:54 -0700

    updated licenses, adding part 7

--- part6/src/main/java/impatient/TfIdfFunction.java
+++ part6/src/main/java/impatient/TfIdfFunction.java
@@ -1,7 +1,21 @@
 /*
  * Copyright (c) 2007-2012 Concurrent, Inc. All Rights Reserved.
  *
- * Project and contact information: http://www.concurrentinc.com/
+ * Project and contact information: http://www.cascading.org/
+ *
+ * This file is part of the Cascading project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package impatient;
@@ -17,7 +31,6 @@ import cascading.tuple.TupleEntry;
 
 public class TfIdfFunction extends BaseOperation implements Function
   {
-
   public TfIdfFunction( Fields fieldDeclaration )
     {
     super( 5, fieldDeclaration );
@@ -26,6 +39,7 @@ public class TfIdfFunction extends BaseOperation implements Function
   public void operate( FlowProcess flowProcess, FunctionCall functionCall )
     {
     TupleEntry argument = functionCall.getArguments();
+
     String doc_id = argument.getString( 0 );
     String token = argument.getString( 1 );
     int tf_count = argument.getInteger( 2 );
