5d8de061c935a3957f69088929ac1a22bc5e6989
Branky Shao <bishao@ebay.com>
2012-11-21 11:22:03 +0800

    use utility method from cascading.test for unit test

--- part6/src/test/java/impatient/ScrubTest.java
+++ part6/src/test/java/impatient/ScrubTest.java
@@ -20,20 +20,40 @@
 
 package impatient;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
+import cascading.CascadingTestCase;
+import cascading.operation.Function;
 import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+import cascading.tuple.TupleListCollector;
 
-public class ScrubTest
-  {
-  @Test
-  public void testMain() throws Exception
-    {
-    ScrubTest tester = new ScrubTest();
-    Fields fieldDeclaration = new Fields( "doc_id", "token" );
-    ScrubFunction scrub = new ScrubFunction( fieldDeclaration );
-
-    assertEquals( "Scrub", "foo bar", scrub.scrubText( "FoO BAR  " ) );
-    }
-  }
+public class ScrubTest extends CascadingTestCase {
+	
+	@Test
+	public void testScrub() {
+		Fields fieldDeclaration = new Fields("doc_id", "token");
+		Function scrub = new ScrubFunction(fieldDeclaration);
+		Tuple[] arguments = new Tuple[] {
+				new Tuple("doc_1", "FoO"),
+				new Tuple("doc_1", " BAR "),
+				new Tuple("doc_1", "     ") // will be scrubed
+		};
+		
+		ArrayList<Tuple> expectResults = new ArrayList<Tuple> ();
+		expectResults.add(new Tuple("doc_1", "foo"));
+		expectResults.add(new Tuple("doc_1", "bar"));
+		
+		TupleListCollector collector = invokeFunction(scrub, arguments, Fields.ALL);
+		Iterator<Tuple> it = collector.iterator();
+		ArrayList<Tuple> results = new ArrayList<Tuple>();
+		while (it.hasNext()) {
+			results.add(it.next());
+		}
+		assertEquals("Scrub result is not expected", expectResults, results);
+		
+	}
+}
