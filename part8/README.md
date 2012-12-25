2f2f4ac0b1371cafbc86f1ededf012e186ea0adc
Paco Nathan <ceteri@gmail.com>
2012-12-24 21:37:07 -0800

    Scalding equivalent of Part 3

---
+++ part8/README.md
@@ -0,0 +1,6 @@
+Scalding Examples
+=================
+
+`Word Count` with a token scrub operation, similar to Part 3:
+
+    scald.rb --local src/scala/Example3.scala --doc data/rain.txt --wc output/wc.tsv
---
+++ part8/src/scala/Example3.scala
@@ -0,0 +1,17 @@
+import com.twitter.scalding._
+
+class Example3(args : Args) extends Job(args) {
+  Tsv(args("doc"), ('doc_id, 'text), skipHeader = true)
+    .read
+    .flatMap('text -> 'token) { text : String => text.split("[ \\[\\]\\(\\),.]") }
+    .mapTo('token -> 'token) { token : String => scrub(token) }
+    .filter('token) { token : String => token.length > 0 }
+    .groupBy('token) { _.size('count) }
+    .write(Tsv(args("wc"), writeHeader = true))
+
+  def scrub(token : String) : String = {
+    token
+      .trim
+      .toLowerCase
+  }  
+}
