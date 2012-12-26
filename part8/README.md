2fd37e65de7c183ef982c2bb85e88f6cf409e059
Paco Nathan <ceteri@gmail.com>
2012-12-25 22:45:45 -0800

    Scalding for Part 4

--- part8/README.md
+++ part8/README.md
@@ -1,10 +1,14 @@
 Scalding Examples
 =================
 
+Example logs of running these apps are in https://gist.github.com/4371896
+
 `Word Count` with a token scrub operation, similar to Part 3:
 
     rm -rf output
     scald.rb --hdfs-local src/scala/Example3.scala --doc data/rain.txt --wc output/wc
 
-An example log of this running is in
-https://gist.github.com/4371896
+`Word Count` with a stop word list based on HashJoin, similar to Part 4:
+
+    rm -rf output
+    scald.rb --hdfs-local src/scala/Example4.scala --doc data/rain.txt --stop data/en.stop --wc output/wc 
---
+++ part8/src/scala/Example4.scala
@@ -0,0 +1,22 @@
+import com.twitter.scalding._
+
+class Example4(args : Args) extends Job(args) {
+  val stopPipe = Tsv(args("stop"), ('stop), skipHeader = true)
+    .read
+
+  Tsv(args("doc"), ('doc_id, 'text), skipHeader = true)
+    .read
+    .flatMap('text -> 'token) { text : String => text.split("[ \\[\\]\\(\\),.]") }
+    .mapTo('token -> 'token) { token : String => scrub(token) }
+    .filter('token) { token : String => token.length > 0 }
+    .leftJoinWithTiny('token -> 'stop, stopPipe)
+    .filter('stop) { stop : String => stop == null }
+    .groupBy('token) { _.size('count) }
+    .write(Tsv(args("wc"), writeHeader = true))
+
+  def scrub(token : String) : String = {
+    token
+      .trim
+      .toLowerCase
+  }  
+}
